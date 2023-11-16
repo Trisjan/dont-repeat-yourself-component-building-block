7dc639a01cc928c6d323e373d4b523d5d15efc12
Trisjan Mustamu <TrisjanM@hotmail.com>
2023-11-16 16:04:57 +0100

    data inladen gelukt + opzetje

---
+++ src/lib/components/Carrousel/Caroussel.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let data
+    console.log(data)
+</script>
+
+<ul class="carousel">
+    {#each data.uitleengeschiedenis1 as book}
+        <li>
+            <img src="{book.image.url}" alt="foto van {book.title}">
+            <section class="rightside">
+                <p>{book.title} / {book.author}</p>
+                <p>{book.author}</p>
+                <p>{book.category}</p>
+                <div>
+                    <p>{book.uitleendatum}</p>
+                    <p>{book.inleverdatum}</p>
+                </div>
+            </section>
+        </li>
+    {/each}
+</ul>
+
+<style>
+
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Caroussel from "$lib/components/Carrousel/Caroussel.svelte";
+    export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Caroussel {data}/>
